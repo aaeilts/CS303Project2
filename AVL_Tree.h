f2c4df69d49efd8b35848bb26acd7b40e324e3be
Andrew <akabmm@mail.umkc.edu>
2020-07-27 05:29:55 -0500

    Work in Progress

--- AVL_Tree.h
+++ AVL_Tree.h
@@ -1,26 +1,78 @@
 #include <algorithm>
 #include <iostream>
-#include "AVL_Node.h"
+#include <sstream>
+#include <list>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+struct File {
+	int size;
+	string name;
+	struct File* left;
+	struct File* right;
+	File operator+(File& file1) {  //Addition overload for adding sizes later
+		File file2;
+		file2.size = size + file1.size;
+		return file2;
+	}
+}*fl;
+bool operator==(File& file1, File& file2) { //Comparison overload for comparing names
+	return (file1.name == file2.name);
+}
+
 class AVL_Tree {
 public:
-	int Height(AVL_Node*);
-	int Difference(AVL_Node*);
-	AVL_Node* RR_Rotate(AVL_Node*);
-	AVL_Node* LL_Rotate(AVL_Node*);
-	AVL_Node* LR_Rotate(AVL_Node*);
-	AVL_Node* RL_Rotate(AVL_Node*);
-	AVL_Node* Balance(AVL_Node*);
-	AVL_Node* Insert(AVL_Node*, int);
-	void Show(AVL_Node*, int);
+	int Height(File*);
+	int Difference(File*);
+	File* RR_Rotate(File*);
+	File* LL_Rotate(File*);
+	File* LR_Rotate(File*);
+	File* RL_Rotate(File*);
+	File* Balance(File*);
+	File* Insert(File*, string, int);				
+	void Show(File*, int);							
 	AVL_Tree() {
-		r = nullptr;
+		fl = nullptr;
 	}
+	void AddFolder(string path, string folderName);				//Required
+	void DeleteFolder(string path, string folderName);			//Required
+	void AddFile(string path, string fileName, int size);		//Required
+	File GetFile(string path, string fileName);					//Required
+	list<File> GetFiles(string path, string fileName);			//Required
+	void DeleteFile(string path, string fileName);				//Required
+
+	vector<string> ParsePath(string path);
 };
 
-int AVL_Tree::Height(AVL_Node* t) {
+//Returns a vector of folder names
+vector<string> AVL_Tree::ParsePath(string path) {
+	vector<string>folders;
+	stringstream ss(path);
+	while (ss.good()) {
+		string substring;
+		getline(ss, substring, '/');
+		folders.push_back(substring);
+	}
+	return folders;
+}
+
+//FIX ME - Adds a folder given the path
+void AVL_Tree::AddFolder(string path, string folderName) {
+	if (path == "none") {
+		fl = Insert(fl, folderName, 0);
+	}
+	else {
+		vector<string> folders;
+		folders = ParsePath(path);
+		//Have folder names, now how to compare with node names and move down to children
+
+	}
+}
+
+
+int AVL_Tree::Height(File* t) {
 	int h = 0;
 	if (t != nullptr) {
 		int lHeight = Height(t->left);
@@ -31,44 +83,44 @@ int AVL_Tree::Height(AVL_Node* t) {
 	return h;
 }
 
-int AVL_Tree::Difference(AVL_Node* t) {
+int AVL_Tree::Difference(File* t) {
 	int lHeight = Height(t->left);
 	int rHeight = Height(t->right);
 	int difference = lHeight - rHeight;
 	return difference;
 }
 
-AVL_Node* AVL_Tree::RR_Rotate(AVL_Node* parent) {
-	AVL_Node* t;
+File* AVL_Tree::RR_Rotate(File* parent) {
+	File* t;
 	t = parent->right;
 	parent->right = t->left;
 	t->left = parent;
 	return t;
 }
 
-AVL_Node* AVL_Tree::LL_Rotate(AVL_Node* parent) {
-	AVL_Node* t;
+File* AVL_Tree::LL_Rotate(File* parent) {
+	File* t;
 	t = parent->left;
 	parent->left = t->right;
 	t->right = parent;
 	return t;
 }
 
-AVL_Node* AVL_Tree::LR_Rotate(AVL_Node* parent) {
-	AVL_Node* t;
+File* AVL_Tree::LR_Rotate(File* parent) {
+	File* t;
 	t = parent->left;
 	parent->left = RR_Rotate(t);
 	return LL_Rotate(parent);
 }
 
-AVL_Node* AVL_Tree::RL_Rotate(AVL_Node* parent) {
-	AVL_Node* t;
+File* AVL_Tree::RL_Rotate(File* parent) {
+	File* t;
 	t = parent->right;
 	parent->right = LL_Rotate(t);
 	return RR_Rotate(parent);
 }
 
-AVL_Node* AVL_Tree::Balance(AVL_Node* t) {
+File* AVL_Tree::Balance(File* t) {
 	int bal_factor = Difference(t);
 	if (bal_factor > 1) {
 		if (Difference(t->left) > 0) {
@@ -91,36 +143,37 @@ AVL_Node* AVL_Tree::Balance(AVL_Node* t) {
 	return t;
 }
 
-AVL_Node* AVL_Tree::Insert(AVL_Node* r, int v) {
+File* AVL_Tree::Insert(File* r, string name, int size) {
 	if (r == nullptr) {
-		r = new AVL_Node;
-		r->d = v;
+		r = new File;
+		r->size = size;
+		r->name = name;
 		r->left = nullptr;
 		r->right = nullptr;
 		return r;
 	}
-	else if (v < r->d) {
-		r->left = Insert(r->left, v);
+	else if (name < r->name) {
+		r->left = Insert(r->left, name, size);
 		r = Balance(r);
 	}
-	else if (v >= r->d) {
-		r->right = Insert(r->right, v);
+	else if (name >= r->name) {
+		r->right = Insert(r->right, name, size);
 		r = Balance(r);
 	}
 	return r;
 }
 
-void AVL_Tree::Show(AVL_Node* p, int left) {
+void AVL_Tree::Show(File* p, int left) {
 	int i;
 	if (p != nullptr) {
 		Show(p->right, left + 1);
 		cout << " ";
-		if (p == r) {
+		if (p == fl) {
 			cout << "Root -> ";
 		}
-		for (i = 0; i < left && p != r; i++) {
+		for (i = 0; i < left && p != fl; i++) {
 			cout << " ";
-			cout << p->d;
+			cout << p->size;
 			Show(p->left, left + 1);
 		}
 	}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <string>
+#include "AVL_Tree.h"
 using namespace std;
 
 int main() {
+	AVL_Tree FileSystem;
 
-
-
-
+	
+	FileSystem.AddFolder("none", "Summer2020");
+	FileSystem.AddFolder("Summer2020", "CS303");
+	
 
 
 
