ed592ca86ad104978e917af2da5e752e96caba6c
aaeilts <60226103+aaeilts@users.noreply.github.com>
2020-07-28 18:34:30 -0500

    Working Tree

--- AVL_Tree.h
+++ AVL_Tree.h
@@ -9,7 +9,7 @@ using namespace std;
 
 struct File {
 	int size;
-	string name;
+	string name, parentFolder;
 	struct File* left;
 	struct File* right;
 	File operator+(File& file1) {  //Addition overload for adding sizes later
@@ -17,11 +17,30 @@ struct File {
 		file2.size = size + file1.size;
 		return file2;
 	}
+	File();
+	//File(string name);
+	File(string parent, string name, int size);
+	
 }*fl;
 bool operator==(File& file1, File& file2) { //Comparison overload for comparing names
 	return (file1.name == file2.name);
 }
+File::File() {};
 
+File::File(string name, string parent = "", int size=0) {
+	this->parentFolder = parent;
+	this->name = name;
+	this->size = size;
+	this->left = NULL;
+	this->right = NULL;
+}
+/*
+File::File(string name) {
+	this->size = 0;
+	this->name = name;
+	this->parentFolder = "";
+}
+*/
 class AVL_Tree {
 public:
 	int Height(File*);
@@ -32,22 +51,20 @@ public:
 	File* RL_Rotate(File*);
 	File* Balance(File*);
 	File* Insert(File*, string, int);				
-	void Show(File*, int);							
-	AVL_Tree() {
-		fl = nullptr;
-	}
-	void AddFolder(string path, string folderName);				//Required
+	void inorder(File*);
+	File* AddFolder(File* t, string path, string folderName);				//Required
 	void DeleteFolder(string path, string folderName);			//Required
 	void AddFile(string path, string fileName, int size);		//Required
 	File GetFile(string path, string fileName);					//Required
 	list<File> GetFiles(string path, string fileName);			//Required
 	void DeleteFile(string path, string fileName);				//Required
+	void AddAttempt(File* t);
 
-	vector<string> ParsePath(string path);
+	string ParsePath(string path);
 };
 
-//Returns a vector of folder names
-vector<string> AVL_Tree::ParsePath(string path) {
+//Returns final folder name
+string AVL_Tree::ParsePath(string path) {
 	vector<string>folders;
 	stringstream ss(path);
 	while (ss.good()) {
@@ -55,26 +72,79 @@ vector<string> AVL_Tree::ParsePath(string path) {
 		getline(ss, substring, '/');
 		folders.push_back(substring);
 	}
-	return folders;
+	return folders.back();
 }
 
-//FIX ME - Adds a folder given the path
-void AVL_Tree::AddFolder(string path, string folderName) {
-	if (path == "none") {
-		fl = Insert(fl, folderName, 0);
+
+/*File AVL_Tree::GetFile(string path, string fileName) {					//Required
+	string folderName;
+	folderName = ParsePath(path);
+
+	return  //return parent folder's size?
+}*/
+
+void AVL_Tree::AddAttempt(File* t) {
+
+}
+
+//Adds a folder given the path
+File* AVL_Tree::AddFolder(File* r, string path, string folderName) {      //Assumption of passing the tree with function
+	string filePath;
+	filePath = ParsePath(path);
+	if (r == NULL) {			//Base Case
+		r = new File;
+		r->parentFolder = filePath;
+		r->name = folderName;
+		r->left = NULL;
+		r->right = NULL;
+		return r;
+	}
+	else if (folderName < r->name) {
+		r->left = AddFolder(r->left, filePath, folderName);
+		r = Balance(r);
+	}
+	else if (folderName >= r->name) {
+		r->right = AddFolder(r->right, filePath, folderName);
+		r = Balance(r);
+	}
+	return r;
+																		  
+																		  
+	/*if (path == "none") {			//Base Case
+		File r;
+		r = File("none", folderName);
 	}
 	else {
-		vector<string> folders;
-		folders = ParsePath(path);
-		//Have folder names, now how to compare with node names and move down to children
-
-	}
+		string parent;
+		parent = ParsePath(path);
+	}*/
 }
 
+
+File* AVL_Tree::Insert(File* r, string name, int size = 0) {
+	if (r == NULL) {			//Base Case
+		r = new File;
+		r->parentFolder = //
+		r->size = size;
+		r->name = name;
+		r->left = NULL;
+		r->right = NULL;
+		return r;
+	}
+	else if (name < r->name) {
+		r->left = Insert(r->left, name, size);
+		r = Balance(r);
+	}
+	else if (name >= r->name) {
+		r->right = Insert(r->right, name, size);
+		r = Balance(r);
+	}
+	return r;
+}
 
 int AVL_Tree::Height(File* t) {
 	int h = 0;
-	if (t != nullptr) {
+	if (t != NULL) {
 		int lHeight = Height(t->left);
 		int rHeight = Height(t->right);
 		int maxHeight = max(lHeight, rHeight);
@@ -143,27 +213,18 @@ File* AVL_Tree::Balance(File* t) {
 	return t;
 }
 
-File* AVL_Tree::Insert(File* r, string name, int size) {
-	if (r == nullptr) {
-		r = new File;
-		r->size = size;
-		r->name = name;
-		r->left = nullptr;
-		r->right = nullptr;
-		return r;
-	}
-	else if (name < r->name) {
-		r->left = Insert(r->left, name, size);
-		r = Balance(r);
-	}
-	else if (name >= r->name) {
-		r->right = Insert(r->right, name, size);
-		r = Balance(r);
-	}
-	return r;
+
+
+void AVL_Tree::inorder(File* t) {		//
+	if (t == NULL)					//
+		return;							//
+	inorder(t->left);					//
+	cout << t->name << " ";				//
+	inorder(t->right);					//
 }
 
-void AVL_Tree::Show(File* p, int left) {
+//FIX ME [Change to compare NAMES instead of size?] [WHAT IS 'int left', is it height of tree?]
+/*void AVL_Tree::Show(File* p, int left) {
 	int i;
 	if (p != nullptr) {
 		Show(p->right, left + 1);
@@ -173,8 +234,8 @@ void AVL_Tree::Show(File* p, int left) {
 		}
 		for (i = 0; i < left && p != fl; i++) {
 			cout << " ";
-			cout << p->size;
+			cout << p->name;                   // Changed from size to name
 			Show(p->left, left + 1);
 		}
 	}
-}
+}*/
--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int main() {
 	AVL_Tree FileSystem;
 
+	//documents/programming/data_struct/project
+
+	fl = FileSystem.AddFolder(fl, "", "documents");
+	//FileSystem.inorder(fl);
+	fl = FileSystem.AddFolder(fl, "documents", "programming");
+	//FileSystem.inorder(fl);
+	fl = FileSystem.AddFolder(fl, "documents/programming", "data_struct");
+	//FileSystem.inorder(fl);
+	fl = FileSystem.AddFolder(fl, "documents/programming/data_struct", "project");
+	FileSystem.inorder(fl);
 	
-	FileSystem.AddFolder("none", "Summer2020");
-	FileSystem.AddFolder("Summer2020", "CS303");
+	
+	//FileSystem.Show(fl, 2); //Testing??
 	
 
 
